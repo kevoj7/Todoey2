60e2eca11cb14553a6417aa2ecd3cea04602171c
Kevin Jorquera <kevinjorquera-08@hotmail.com>
2020-05-23 18:35:14 -0400

    Initial Commit

---
+++ Todoey2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey2
+//
+//  Created by kevin jorquera on 5/23/20.
+//  Copyright © 2020 Kevin Jorquera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
